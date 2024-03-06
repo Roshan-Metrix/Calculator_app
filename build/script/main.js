02750816ab6acb0c5fbecf0baf24c199be4b2f01
Roshan Patel <roshanpatel12309@gmail.com>
2024-03-06 14:46:28 +0545

    Btn function activated

--- build/script/main.js
+++ build/script/main.js
@@ -1,8 +1,31 @@
 const modeChange = document.getElementById("theme");
 const body = document.getElementById("main");
+const display = document.getElementById("calc");
+const btns = document.querySelectorAll('button');
+
+let string = '';
+//taking button value
+const btnArray = Array.from(btns);
+btnArray.forEach((event)=>{
+  event.addEventListener("click",(e)=>{
+    if(e.target.innerHTML == 'C'){
+      string = string.substring(0,string.length-1);
+      display.innerHTML=string;
+    }else if(e.target.innerHTML == 'AC'){
+      string = string.substring(0,string.length = 0);
+      display.innerHTML = string;
+    }
+    else{
+    string += e.target.innerHTML;
+    display.innerHTML = string;
+    }
+  });
+});
+
 
 //dark/light mode setup
 modeChange.addEventListener("click",()=>{
   body.classList.toggle("dark");
 });
+
 
